ee2ecc43aa342744956a1dd04459a37f80fd37ff
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-02-13 15:48:43 +0300

    Add files via upload

---
+++ dz2.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Rus");
+    int a, b;
+    cout << "Введите первое число: " << endl;
+    cin >> a;
+    cout << "Введите второе число: " << endl;
+    cin >> b;
+    if (a > b)
+        cout << "Больше." << endl;
+    else if (a < b)
+        cout << "Меньше." << endl;
+    else
+        cout << "Равны." << endl;
+}
